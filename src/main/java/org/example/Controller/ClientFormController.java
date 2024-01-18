2f11c43548cf110baaccde5651fd3988be4377fc
theekshana-dilshan <sptheekshanadilshan@gmail.com>
2024-01-18 23:38:37 +0530

    emojies

--- src/main/java/org/example/Controller/ClientFormController.java
+++ src/main/java/org/example/Controller/ClientFormController.java
@@ -1,26 +1,35 @@
 package org.example.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class ClientFormController {
 
     @FXML
+    private AnchorPane emojiPane;
+
+    @FXML
     private AnchorPane root;
 
+    @FXML
+    private TextField txtMessage;
 
     @FXML
     private Pane supRoot;
 
     public void initialize(){
         supRoot.setVisible(false);
+        emojiPane.setVisible(!emojiPane.isVisible());
     }
+
+
 
     @FXML
     void imgBackOnAction(MouseEvent event) throws IOException {
@@ -36,5 +45,51 @@ public class ClientFormController {
     @FXML
     void btnOptionOnAction(javafx.event.ActionEvent actionEvent) {
         supRoot.setVisible(true);
+    }
+
+    @FXML
+    void btnImojiOnAction(ActionEvent actionEvent) {
+        emojiPane.setVisible(!emojiPane.isVisible());
+    }
+
+    public void thumbsUpOnAction(MouseEvent mouseEvent) {
+        txtMessage.appendText("\uD83D\uDC4D");
+        emojiPane.setVisible(false);
+    }
+
+    public void hartOnAction(MouseEvent mouseEvent) {
+        txtMessage.appendText("\u2600\u26FF");
+        emojiPane.setVisible(false);
+    }
+
+    public void sunglassOnFaceOnAction(MouseEvent mouseEvent) {
+        txtMessage.appendText("\uD83D\uDE0E");
+        emojiPane.setVisible(false);
+    }
+
+    public void angryMoodOnAction(MouseEvent mouseEvent) {
+        txtMessage.appendText("\uD83D\uDE08");
+        emojiPane.setVisible(false);
+    }
+
+    public void hartFaceOnAction(MouseEvent mouseEvent) {
+        txtMessage.appendText("\uD83D\uDE0D");
+        emojiPane.setVisible(false);
+    }
+
+    public void kissOnAction(MouseEvent mouseEvent) {
+        txtMessage.appendText("\uD83D\uDE1A");
+        emojiPane.setVisible(false);
+    }
+
+    public void smileMoodOnAction(MouseEvent mouseEvent) {
+        txtMessage.appendText("\uD83D\uDE02");
+        emojiPane.setVisible(false);
+    }
+
+
+    public void laughFaceOnAction(MouseEvent mouseEvent) {
+        txtMessage.appendText("\uD83D\uDE02");
+        emojiPane.setVisible(false);
     }
 }
