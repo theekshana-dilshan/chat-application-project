9a1e3845fa695593c273c8814515eb676dc5e6b5
theekshana-dilshan <sptheekshanadilshan@gmail.com>
2024-01-19 01:33:42 +0530

    updated

--- src/main/java/org/example/Controller/ClientFormController.java
+++ src/main/java/org/example/Controller/ClientFormController.java
@@ -4,10 +4,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ClientFormController {
@@ -24,6 +30,11 @@ public class ClientFormController {
     @FXML
     private Pane supRoot;
 
+    @FXML
+    private ImageView imgWallpaper;
+
+    private File file;
+
     public void initialize(){
         supRoot.setVisible(false);
         emojiPane.setVisible(!emojiPane.isVisible());
@@ -38,13 +49,8 @@ public class ClientFormController {
     }
 
     @FXML
-    void lblCloseOnAction(MouseEvent event) {
-        supRoot.setVisible(false);
-    }
-
-    @FXML
     void btnOptionOnAction(javafx.event.ActionEvent actionEvent) {
-        supRoot.setVisible(true);
+        supRoot.setVisible(!supRoot.isVisible());
     }
 
     @FXML
@@ -91,5 +97,29 @@ public class ClientFormController {
     public void laughFaceOnAction(MouseEvent mouseEvent) {
         txtMessage.appendText("\uD83D\uDE02");
         emojiPane.setVisible(false);
+    }
+
+    public void setWallpaperOnAction(MouseEvent mouseEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select the image");
+        FileChooser.ExtensionFilter imageFilter =
+                new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.jpeg", "*.png", "*.gif", "*.bmp");
+        fileChooser.getExtensionFilters().add(imageFilter);
+
+        File file = fileChooser.showOpenDialog(imgWallpaper.getScene().getWindow());
+
+        if (file != null) {
+            imgWallpaper.setImage(new Image(file.toURI().toString()));
+            supRoot.setVisible(!supRoot.isVisible());
+        }
+    }
+
+    public void btnFileOnAction(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select the image");
+        FileChooser.ExtensionFilter imageFilter =
+                new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.jpeg", "*.png", "*.gif");
+        fileChooser.getExtensionFilters().add(imageFilter);
+        file = fileChooser.showOpenDialog(txtMessage.getScene().getWindow());
     }
 }
